f1f23276239a6abd3f2d92af55d8de7cf4e6efa4
MD-Hamza <muhammadhamza0403@gmail.com>
2025-08-21 11:44:30 -0400

    https://codeforces.com/contest/2132/problem/B

---
+++ Div3/1043/SecretNumber.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* clang-format off */
+// https://codeforces.com/contest/2132/problem/B
+
+/* TYPES  */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+
+/* UTILS */
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+/*  All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+ll power(ll a, ll b) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+void solve()
+{
+    ll n, t;
+    cin >> n;
+    vector<ll> s;
+    for (int i = 18; i >= 1; i--) {
+        t = power(10, i) + 1;
+        if (n % t == 0) s.push_back(n / t);
+    }
+    cout << s.size() << endl;
+    for (ll j : s) cout << j << " ";
+    if (s.size()) cout << "\n";
+
+}
+
+/* Main()  function */
+int32_t main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int _tc = 1;
+    cin >> _tc;
+
+    for (int i = 1; i <= _tc; ++i)
+    {
+        solve();
+    }
+}
