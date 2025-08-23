b6ae06746e8c9c8ac4ef7c693cf3bc200d0cd4a8
MD-Hamza <muhammadhamza0403@gmail.com>
2025-08-22 21:55:46 -0400

    https://codeforces.com/contest/2132/problem/C2

---
+++ Div3/1043/CunningSellerHard.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* clang-format off */
+// https://codeforces.com/contest/2132/problem/C2
+
+/* TYPES  */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+
+/* UTILS */
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+/*  All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+
+void solve()
+{
+    ll n, c, j, y, k;
+    cin >> n >> k;
+
+    c = 0;
+    j = 3;
+    y = 1;
+
+    while (n > 0) {
+        if (n % 3 != 0) c += (j * (n % 3));
+        n /= 3;
+        j = j * 3 + y;
+        y *= 3;
+    }
+    cout << c << endl;
+}
+
+/* Main()  function */
+int32_t main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int _tc = 1;
+    cin >> _tc;
+
+    for (int i = 1; i <= _tc; ++i)
+    {
+        solve();
+    }
+}
