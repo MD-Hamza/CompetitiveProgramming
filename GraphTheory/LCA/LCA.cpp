5c67ecee45074aa38abf89563846f2f6e3422b86
MD-Hamza <muhammadhamza0403@gmail.com>
2025-08-25 21:28:11 -0400

    https://codeforces.com/contest/208/problem/E

--- GraphTheory/LCA/LCA.cpp
+++ GraphTheory/LCA/LCA.cpp
@@ -32,10 +32,71 @@ typedef unsigned long int uint32;
 typedef long long int int64;
 typedef unsigned long long int  uint64;
 
+vector<vector<int>> up;
+vector<int> parents;
+vector<vector<int>> children;
+
+int n, l;
+
+void preprocess()
+{
+    for (int i = 1; i <= n; i++)
+        up[i][0] = parents[i - 1];
+    
+    for (int j = 1; j <= l; j++) {
+        for (int i = 1; i <= n; i++) {
+            up[i][j] = up[up[i][j - 1]][j - 1];
+        }
+    }
+}
+
+int ancestors(int u, int p) {
+    queue<int> q;
+    q.push(u);
+    int depth = 0;
+    while (!q.empty()) {
+        for (int i = q.size(); i >= 0; i--) {
+            depth++;
+            int v = q.front(); q.pop();
+            for (int m : children[v]) {
+                q.push(m);
+            }
+        }
+        cout << depth << endl;
+        if (depth == p)
+            return q.size();
+    }
+    return 0;
+}
 
 void solve()
 {
+    int m, p, v;
+    cin >> n;
 
+    l = ceil(log2(n + 1));
+    up.assign(n + 1, vector<int>(l + 1));
+    children.resize(n + 1);
+
+    for (int i = 0; i < n; i++) {
+        cin >> p;
+        parents.push_back(p);
+        children[p].push_back(i + 1);
+    }
+    
+    preprocess();
+    cin >> m;
+
+    for (int j = 0; j < m; j++) {
+        cin >> v >> p;
+        for (int j = l; j >= 0; j--) {
+            if (p & (1 << j)) {
+                v = up[v][j];
+            }
+        }
+        if (v == 0) cout << 0 << " ";
+        else cout << ancestors(v, p) << " ";
+    }
 }
 
 /* Main()  function */
@@ -43,7 +104,7 @@ int32_t main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
 
     int _tc = 1;
-    cin >> _tc;
+    //cin >> _tc;
 
     for (int i = 1; i <= _tc; ++i)
     {
