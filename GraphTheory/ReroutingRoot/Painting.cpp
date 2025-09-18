7ebe16a799d73a6801d5b16a16b227db220dd593
MD-Hamza <muhammadhamza0403@gmail.com>
2025-09-18 03:18:15 -0400

    https://codeforces.com/contest/1187/problem/E

--- GraphTheory/ReroutingRoot/Painting.cpp
+++ GraphTheory/ReroutingRoot/Painting.cpp
@@ -32,10 +32,55 @@ typedef unsigned long int uint32;
 typedef long long int int64;
 typedef unsigned long long int  uint64;
 
+vector<vector<int>> adj;
+
+ll dfs(int root, int p, vector<ll> &sub) {
+    ll s = 0;
+    for (auto v : adj[root]) {
+        if (v == p) continue;
+        s += dfs(v, root, sub);
+        sub[root] += sub[v];
+    }
+
+    sub[root]++;
+    return s + sub[root];
+}
+
+ll maxSum(int root, int p, vector<ll> &sub, ll s) {
+    ll maxS = s;
+    for (auto v : adj[root]) {
+        if (v == p) continue;
+        ll tempRoot = sub[root];
+        ll tempV = sub[v];
+
+        sub[v] = sub[root];
+        sub[root] -= tempV;
+
+        maxS = max(maxS, maxSum(v, root, sub, s - 2 * tempV + tempRoot));
+
+        sub[root] = tempRoot;
+        sub[v] = tempV;
+    }
+
+    return maxS;
+}
 
 void solve()
 {
+    int n, u, v;
+    cin >> n;
+    adj.resize(n);
 
+    for (int i = 0; i < n - 1; i++) {
+        cin >> u >> v;
+        adj[u - 1].push_back(v - 1);
+        adj[v - 1].push_back(u - 1);
+    }
+
+    vector<ll> sub(n);
+    ll s = dfs(0, 0, sub);
+
+    cout << maxSum(0, 0, sub, s) << endl; 
 }
 
 /* Main()  function */
@@ -43,7 +88,7 @@ int32_t main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
 
     int _tc = 1;
-    cin >> _tc;
+    //cin >> _tc;
 
     for (int i = 1; i <= _tc; ++i)
     {
