553c38f4c5e4d2d62620e207912f914b8fdaafb2
MD-Hamza <muhammadhamza0403@gmail.com>
2025-11-21 21:16:16 -0500

    https://codeforces.com/contest/2171/problem/C2

---
+++ Div3/1065/XORGameHard.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* clang-format off */
+
+/* TYPES  */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+
+/* UTILS */
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+/*  All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    vector<int> b(n);
+    ll mWin = 0;
+    ll aWin = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        aWin = aWin ^ a[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+        mWin = mWin ^ b[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        ll tempA = aWin ^ b[i] ^ a[i];
+        ll tempM = mWin ^ b[i] ^ a[i];
+        if (i % 2 == 0 && tempA > tempM || i % 2 == 1 && tempA < tempM) {
+            aWin = tempA;
+            mWin = tempM;
+        }
+    }
+
+    if (aWin > mWin) cout << "Ajisai" << endl;
+    else if (aWin < mWin) cout << "Mai" << endl;
+    else cout << "Tie" << endl;
+}
+
+/* Main()  function */
+int32_t main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int _tc = 1;
+    cin >> _tc;
+
+    for (int i = 1; i <= _tc; ++i)
+    {
+        solve();
+    }
+}
