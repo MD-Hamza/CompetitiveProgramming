f237bfa489430ef2fc1f5d32d33c3b66a550e23f
MD-Hamza <muhammadhamza0403@gmail.com>
2025-12-23 23:30:44 -0500

    https://codeforces.com/contest/2179/problem/D

---
+++ Div3/1071/D.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* clang-format off */
+
+/* TYPES  */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+
+/* UTILS */
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+/*  All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    ll m = pow(2, n) - 1;
+    cout << m << " ";
+    unordered_set<ll> c;
+
+    ll x = m;
+    for (ll i = 0; i < n - 1; i++) {
+        x >>= 1;
+        for (ll j = x; j < m; j++) {
+            if (c.count(j)) continue;
+            if ((j & x) == x) {
+                cout << j << " ";
+                c.insert(j);
+            }
+        }    
+    }
+
+    for (ll i = 0; i < m; i++) {
+        if (c.count(i)) continue;
+        cout << i << " ";
+    }
+    cout << "\n";
+}
+
+/* Main()  function */
+int32_t main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int _tc = 1;
+    cin >> _tc;
+
+    for (int i = 1; i <= _tc; ++i)
+    {
+        solve();
+    }
+}
