09e3d4d87324e4b00760293377ed68aa6f5a0e81
MD-Hamza <muhammadhamza0403@gmail.com>
2026-01-01 23:57:54 -0500

    https://codeforces.com/contest/2178/problem/C

--- 2025Contest/C.cpp
+++ 2025Contest/C.cpp
@@ -34,7 +34,11 @@ typedef unsigned long long int  uint64;
 
 void solve()
 {
+    int n;
+    cin >> n;
+    vector<int> a(n);
 
+    for (int i = 0; i < n; i++) cin >> a[i];
 }
 
 /* Main()  function */
